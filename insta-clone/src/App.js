30aa594a39c525342fa2796ffa711562f1d16df9
Jeff Oliver <jeffoliver3384@gmail.com>
2019-06-05 16:08:19 -0400

    WIP: added HOC withAuthenticate and rendering posts page

--- insta-clone/src/App.js
+++ insta-clone/src/App.js
@@ -1,77 +1,14 @@
 import React, { Component } from 'react';
-import { Container, Navbar } from 'react-bootstrap';
-import dummyData from './dummy-data';
-import SearchBar from './Components/SearchBar/SearchBar';
-import PostContainer from './Components/PostContainer/PostContainer';
+import PostsPage from './Components/PostContainer/PostsPage';
+import WithAuthenticate from './Authentication/WithAuthenticate';
 import './App.css';
+import withAuthenticate from './Authentication/WithAuthenticate';
+
+const ComponentFromWithAuthenticate = withAuthenticate(PostsPage);
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: []
-    };
-  }
-
-  componentDidMount() {
-    this.setState({
-      data: dummyData
-    });
-  }
-
-  toggleLike = id => {
-    const updateData = this.state.data.map(post => {
-      if (post.id === id) {
-        if (post.liked === true) {
-          post.likes -= 1;
-          post.liked = false;
-          return post;
-        } else {
-          post.likes += 1;
-          post.liked = true;
-          return post;
-        }
-      } else {
-        return post;
-      }
-    });
-
-    this.setState({
-      data: updateData
-    });
-
-    console.log(this.state.data);
-  };
-
-  filterSearch = search => {
-    const filterData = this.state.data.filter(post => {
-      if (post.username === search) {
-        return post;
-      }
-    });
-
-    this.setState({
-      data: filterData
-    });
-  };
-
   render() {
-    return (
-      <Container className="App">
-        <Navbar>
-          <SearchBar filterSearch={this.filterSearch} />
-        </Navbar>
-        {this.state.data.map(post => {
-          return (
-            <PostContainer
-              key={post.id}
-              post={post}
-              toggleLike={this.toggleLike}
-            />
-          );
-        })}
-      </Container>
-    );
+    return <ComponentFromWithAuthenticate />;
   }
 }
 
---
+++ insta-clone/src/Components/PostContainer/PostsPage.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Container, Navbar } from 'react-bootstrap';
+import dummyData from '../../dummy-data';
+import SearchBar from '../SearchBar/SearchBar';
+import PostContainer from '../PostContainer/PostContainer';
+import '../../App.css';
+
+class PostsPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      data: dummyData
+    });
+  }
+
+  toggleLike = id => {
+    const updateData = this.state.data.map(post => {
+      if (post.id === id) {
+        if (post.liked === true) {
+          post.likes -= 1;
+          post.liked = false;
+          return post;
+        } else {
+          post.likes += 1;
+          post.liked = true;
+          return post;
+        }
+      } else {
+        return post;
+      }
+    });
+
+    this.setState({
+      data: updateData
+    });
+
+    console.log(this.state.data);
+  };
+
+  filterSearch = search => {
+    const filterData = this.state.data.filter(post => {
+      if (post.username === search) {
+        return post;
+      }
+    });
+
+    this.setState({
+      data: filterData
+    });
+  };
+
+  render() {
+    return (
+      <Container className="App">
+        <Navbar>
+          <SearchBar filterSearch={this.filterSearch} />
+        </Navbar>
+        {this.state.data.map(post => {
+          return (
+            <PostContainer
+              key={post.id}
+              post={post}
+              toggleLike={this.toggleLike}
+            />
+          );
+        })}
+      </Container>
+    );
+  }
+}
+
+export default PostsPage;
